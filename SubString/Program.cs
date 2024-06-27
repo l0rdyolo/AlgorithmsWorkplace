fdbfaf136d78b08fdfb1799a37f7a2268049c8f7
l0rdyolo <wp.omer820@gmail.com>
2024-06-27 18:18:04 +0300

    substring

---
+++ SubString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace SubString;
+
+class Program
+{
+    static bool IsSubString(string s1, string s2) {
+        int l_s1 = s1.Length + 1; 
+        int l_s2 = s2.Length + 1;
+        int[,] matrix = new int[l_s1, l_s2];
+        int maxValue = 0;
+
+        for (int i = 1; i < l_s1; i++) {
+            for (int j = 1; j < l_s2; j++) {
+                
+                if (s1[i - 1] == s2[j - 1]) {
+                    matrix[i, j] = matrix[i - 1, j - 1] + 1;
+                    if (matrix[i, j] > maxValue) {
+                        maxValue = matrix[i, j];
+                    }
+                }
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        return maxValue == s2.Length; 
+    }
+
+    static void Main(string[] args) {
+        string s1 = "abcdefg";
+        string s2 = "bcde";
+
+        Console.WriteLine(IsSubString(s1, s2));
+    }
+}
--- SubString/obj/Debug/net8.0/SubString.AssemblyInfo.cs
+++ SubString/obj/Debug/net8.0/SubString.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("NQueensProblem")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("SubString")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33e51f8bb4876405c6cf3c9d9aef3d4abfc7f0e2")]
-[assembly: System.Reflection.AssemblyProductAttribute("NQueensProblem")]
-[assembly: System.Reflection.AssemblyTitleAttribute("NQueensProblem")]
+[assembly: System.Reflection.AssemblyProductAttribute("SubString")]
+[assembly: System.Reflection.AssemblyTitleAttribute("SubString")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
